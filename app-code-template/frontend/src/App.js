d5834909b447cc3a33a79aaa982169ea3bbf77e7
mason <masonwong1999@gmail.com>
2024-07-16 12:27:24 +1000

    added red page

---
+++ app-code-template/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello from red page
+      </header>
+    </div>
+  );
+}
+
+export default App;
